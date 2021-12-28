911d104724dfaa1b69c2694164c392a1611a5719
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-28 09:29:32 +0530

    Delete Routes directory

--- Routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const service = require("../Services/users.service");
-
-router.post("/register", service.register);
-
-router.post("/login", service.login);
-
-module.exports = router;
