6a2d364f9f857755498d7672d5746d87b44428b0
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-28 09:22:47 +0530

    Add files via upload

---
+++ Shared/mongo.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+// const URL = "mongodb://localhost:27017";
+const DB_NAME = process.env.DB_NAME;
+const URL = process.env.URL;
+// const DB_NAME = process.env.DB_NAME;
+
+const client = new MongoClient(URL); //obj literals
+// console.log(URL);
+
+module.exports = {
+  db: null,
+  register: null,
+
+  async connect() {
+    await client.connect(); // obj function from db driver
+    console.log("mongo db connected using driver", URL);
+
+    this.db = client.db(DB_NAME);
+    console.log("DB selected ", DB_NAME);
+    this.register = this.db.collection("register"); //collection choose
+  },
+};
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const cors = require("cors");
+const mongo = require("./Shared/mongo");
+
+(async () => {
+    
+    await mongo.connect(); //connection db
+    app.use(cors());
+    app.use(express.json()); //parsing string to json
+
+    //routes
+    app.use("/users", userRoute);
+    app.use("/resetpassword", resetRoute);
+
+    app.get("/", (req, res) =>
+      res.send(`PASSWORD-RESET-TASK`)
+     );
+
+    const port = process.env.PORT || 6001;
+    app.listen(port, () => console.log(`App listen on port ${port}`));
+   
+})();
+const userRoute = require("./Routes/users.routes");
+const resetRoute = require("./Routes/resetPass.routes");
